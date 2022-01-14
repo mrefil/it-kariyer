6604c364d28e20dd5f45bf490680e36cbe2caecc
Mustafa E <61510151+mrefil@users.noreply.github.com>
2022-01-14 00:40:14 -0500

    fix change password error

--- pages/profile/change-password.js
+++ pages/profile/change-password.js
@@ -3,10 +3,22 @@ import Sidebar from "../../components/layout/sidebar";
 import ChangePasswordHandler from "../../components/profile/change-password/profile-form";
 
 function ChangePassword() {
+    async function changePasswordHandler(passwordData) {
+        const response = await fetch('/api/user/change-password', {
+            method: 'PATCH',
+            body: JSON.stringify(passwordData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+    }
+
     return (
         <Fragment>
             <Sidebar />
-            <ChangePasswordHandler />
+            <ChangePasswordHandler onChangePassword={changePasswordHandler} />
         </Fragment>
     )
 }
