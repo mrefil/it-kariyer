4ec8cba39105bba9016086d23bb534e8281e7a2f
Mustafa E <61510151+mrefil@users.noreply.github.com>
2022-01-14 00:07:09 -0500

    fix change password styles

--- components/profile/change-password/profile-form.js
+++ components/profile/change-password/profile-form.js
@@ -18,19 +18,23 @@ function ProfileForm(props) {
   }
 
   return (
-    <form className={classes.form} onSubmit={submitHandler}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' ref={newPasswordRef} />
+    <section className={classes.auth}>
+      <div className={classes.form_wrapper}>
+        <div className={classes.wrapper}>
+          <form className={classes.form} onSubmit={submitHandler}>
+            <div className={classes.control}>
+              <input type='password' id='new-password' placeholder="New Password" ref={newPasswordRef} />
+            </div>
+            <div className={classes.control}>
+              <input type='password' id='old-password' placeholder="Old Password" ref={oldPasswordRef} />
+            </div>
+            <div className={classes.action}>
+              <button>Change Password</button>
+            </div>
+          </form>
+        </div>
       </div>
-      <div className={classes.control}>
-        <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' ref={oldPasswordRef} />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
+    </section>
   );
 }
 
